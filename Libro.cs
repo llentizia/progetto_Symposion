b25f0bf46e85692a538cc7a5a704a20df284281a
Letizia Mancini <38510317+llentizia@users.noreply.github.com>
2018-06-14 15:01:06 +0200

    Add files via upload

---
+++ Libro.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public enum Genre
+    {
+        Avventura,
+        Classici,
+        Fantascienza,
+        Fantasy,
+        Giallo,
+        Horror,
+        Personale,
+        Romanzo,
+        Thriller,
+        Umorismo,
+    }
+
+    class Libro
+    {
+        
+
+        string _titolo;
+        string _autore;
+        int _anno;
+        Genre _genere;
+
+
+        public Libro(string titolo, string autore, int? anno, string genereString)
+        {
+            if (String.IsNullOrEmpty(titolo))
+                throw new ArgumentException("Titolo nullo");
+            if (String.IsNullOrEmpty(autore))
+                throw new ArgumentException("Autore nullo");
+            if (anno == null)
+                throw new ArgumentException("Anno nullo");
+            if (String.IsNullOrEmpty(genereString))
+                throw new ArgumentException("Genere nullo");
+
+            _titolo = titolo;
+            _autore = autore;
+            _anno = (int)anno;
+
+            if (Enum.TryParse(genereString, out _genere))
+                if (!Enum.IsDefined(typeof(Genre), _genere))
+                    throw new ArgumentException("Genere non valido");
+        }
+
+        public string Titolo
+        {
+            set; get; 
+        }
+
+        public string Autore
+        {
+            set
+            {
+                _autore = value;
+            }
+            get
+            {
+                return _autore;
+            }
+        }
+
+        public int Anno
+        {
+            set
+            {
+                _anno = value;
+            }
+            get
+            {
+                return _anno;
+            }
+        }
+
+        public Genre Genere
+        {
+            set
+            {
+                _genere = value;
+            }
+            get
+            {
+                return _genere;
+            }
+        }
+    }
+}
---
+++ Lista.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libreria
+{
+    public partial class Lista : Form
+    {
+        string _nome;
+        string _categoria;
+        bool _visibilita;
+
+        public Lista(string nome, string categoria)
+        {
+            if (String.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome nullo");
+            if (String.IsNullOrEmpty(categoria))
+                throw new ArgumentException("Categoria nulla");
+            _nome = nome;
+            _categoria = categoria;
+            _visibilita = false;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public string Categoria
+        {
+            get
+            {
+                return _categoria;
+            }
+            set
+            {
+                _categoria = value;
+            }
+        }
+
+        public bool Visibilita
+        {
+            get
+            {
+                return _visibilita;
+            }
+            set
+            {
+                _visibilita = value;
+            }
+        }
+
+    }
+}
